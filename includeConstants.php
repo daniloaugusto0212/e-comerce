21b9555eeb7b945bf853a6d4a21c3a18ba55b634
Danilo <daniloaugusto0212@hotmail.com>
2020-05-25 16:30:58 -0300

    first commit

---
+++ includeConstants.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+date_default_timezone_set('America/Sao_Paulo');
+require('vendor/autoload.php');
+$autoload = function($class){
+    if($class == 'Email'){
+      include_once('classes/phpmailer/PHPMailerAutoload.php');  
+    }
+    include('classes/'.$class.'.php');
+    
+};
+
+spl_autoload_register($autoload);
+
+//Localhost 
+    define('INCLUDE_PATH','http://localhost/gestao_clientes/');
+    define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
+
+    define('BASE_DIR_PAINEL',__DIR__.'../painel');
+    define('HOST','localhost');
+    define('USER','root');
+    define('PASSWORD','');
+    define('DATABASE','sistema');
+    define ('NOME_EMPRESA','Dansol');
+
+
+    ?>
